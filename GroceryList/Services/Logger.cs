f21830286e1e8ffd82a43676156d479bd14bafea
Tobias Johansson <tobias.x.johansson@combitech.se>
2014-12-01 11:18:28 +0100

    Added logger

---
+++ GroceryList/Services/Logger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+
+namespace GroceryList.Services
+{
+    public class Logger : ILogger
+    {
+        public void LogEvent(string message)
+        {
+            Debug.WriteLine(message);
+        }
+    }
+}
